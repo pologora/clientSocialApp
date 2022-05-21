f7a3e69aef360e221900d81633995461ffb5498c
Oleksandr <lysakova@yahoo.com>
2022-05-21 17:36:02 +0200

    remove app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
-import './app.css'
 import Header from './components/Header/Header'
 import { useDispatch } from 'react-redux'
 import { useEffect, useState } from 'react'
